14fc3e2f9d77669bc5a4e73701df896fffa9527f
Helen <helen.chirko@gmail.com>
2022-04-06 15:44:37 +0300

    my first commit from workplace

---
+++ lesson3_6/test_items.py
@@ -0,0 +1,7 @@
+import pytest
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_find_button(browser):
+    browser.get(link)
+    assert browser.find_element_by_css_selector('.btn-add-to-basket')
